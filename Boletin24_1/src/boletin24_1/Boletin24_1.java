5405ce6e5ac24352133255fabc0d808a1d6b8aab
Jano <Jano@192.168.1.90>
2016-04-05 17:29:05 +0200

    Boletín 24

---
+++ Boletin24_1/src/boletin24_1/Boletin24_1.java
@@ -0,0 +1,19 @@
+
+package boletin24_1;
+
+/**
+ *
+ * @author Jano
+ */
+public class Boletin24_1 {
+    public static void main(String[] args) {
+        
+        Menu menu1=new Menu();
+        menu1.setVisible(true);
+        
+        
+        
+        
+    }
+    
+}
---
+++ Boletin24_2/src/boletin24_2/Boletin24_2.java
@@ -0,0 +1,16 @@
+
+package boletin24_2;
+/**
+ *
+ * @author Jano
+ */
+public class Boletin24_2 {
+    public static void main(String[] args) {
+       
+        Menu menu1=new Menu();
+        menu1.setVisible(true);
+        
+        
+    }
+    
+}
